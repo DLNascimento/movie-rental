eb4bd9ac163f92a7cd081e4a3d2fa6f4c12f2897
Diego Nascimento <nascimentodln22@gmail.com>
2026-03-24 11:36:35 -0300

    Added user controller with the POST method for creating a new user.

---
+++ src/main/java/com/diego/rental/user/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.diego.rental.user.controller;
+
+import com.diego.rental.user.dto.UserRequestDTO;
+import com.diego.rental.user.dto.UserResponseDTO;
+import com.diego.rental.user.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponseDTO> create(@RequestBody @Valid UserRequestDTO request){
+        UserResponseDTO response = service.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+    }
+
+}
