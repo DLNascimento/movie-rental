74c66d592c1798433a09ff9e69d0bb82c6edd6e8
Diego Nascimento <nascimentodln22@gmail.com>
2026-03-24 11:35:10 -0300

    adding service ft create user

---
+++ src/main/java/com/diego/rental/user/service/UserService.java
@@ -0,0 +1,47 @@
+package com.diego.rental.user.service;
+
+import com.diego.rental.user.dto.UserRequestDTO;
+import com.diego.rental.user.dto.UserResponseDTO;
+import com.diego.rental.user.entity.UserEntity;
+import com.diego.rental.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public UserResponseDTO create(UserRequestDTO requestDTO){
+
+        userRepository.findByEmail(requestDTO.email()).ifPresent(user -> {
+            throw new RuntimeException("Email already exists");
+        });
+
+        userRepository.findByCpf(requestDTO.cpf()).ifPresent(cpf -> {
+            throw new RuntimeException("Cpf already exists");
+        });
+
+        UserEntity user = new UserEntity(
+                requestDTO.name(),
+                requestDTO.lastName(),
+                requestDTO.email(),
+                requestDTO.cpf()
+        );
+        UserEntity saved = userRepository.save(user);
+
+        return new UserResponseDTO(
+                saved.getId(),
+                saved.getName(),
+                saved.getLastName(),
+                saved.getEmail()
+        );
+
+    }
+
+
+}
