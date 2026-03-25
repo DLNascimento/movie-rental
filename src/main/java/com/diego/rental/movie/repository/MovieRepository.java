dde042530858a890d9670fbb0a634d8a33466166
Diego Nascimento <nascimentodln22@gmail.com>
2026-03-25 11:18:30 -0300

    adding movie repository

---
+++ src/main/java/com/diego/rental/movie/repository/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.diego.rental.movie.repository;
+
+import com.diego.rental.movie.entity.MovieEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<MovieEntity, Long> {
+}
