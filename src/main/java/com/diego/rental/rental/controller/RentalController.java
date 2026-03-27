2b2fa8cb1b769053701373cd4c16e1695e4e97e0
Diego Nascimento <nascimentodln22@gmail.com>
2026-03-27 10:31:51 -0300

    adding rental controller for rental and return

---
+++ src/main/java/com/diego/rental/rental/controller/RentalController.java
@@ -0,0 +1,37 @@
+package com.diego.rental.rental.controller;
+
+import com.diego.rental.movie.dto.CreateMovieDTO;
+import com.diego.rental.rental.dto.CreateRentalDTO;
+import com.diego.rental.rental.dto.RentalResponseDTO;
+import com.diego.rental.rental.service.RentalService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/rentals")
+public class RentalController {
+
+    private final RentalService rentalService;
+
+    public RentalController(RentalService rentalService) {
+        this.rentalService = rentalService;
+    }
+
+    @PostMapping
+    public ResponseEntity<RentalResponseDTO> rent(@RequestBody CreateRentalDTO dto){
+
+        RentalResponseDTO response = rentalService.rentMovie(dto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PostMapping("/{id}/return")
+    public ResponseEntity<RentalResponseDTO> returnMovie(@PathVariable Long id){
+
+        RentalResponseDTO response = rentalService.returnMovie(id);
+
+        return ResponseEntity.ok(response);
+
+    }
+}
